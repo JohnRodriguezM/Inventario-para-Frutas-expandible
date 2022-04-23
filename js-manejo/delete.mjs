fbad8afb2b2a5a6457c7925ace4415e8867ed197
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-04-22 20:09:28 -0500

    fixed delete

--- js-manejo/delete.mjs
+++ js-manejo/delete.mjs
@@ -12,6 +12,7 @@ d.addEventListener("click", e => {
       let isDelete = confirm("Are you sure you want to delete?");
       // se hace uso del operador ternario
       isDelete ? fetch(`${url}/${e.target.dataset.id}`, options) : console.warn(' no eliminado')
+      location.reload()
     }
     catch (err) { console.error('se produjo un', err) }
     finally { console.log('se ha usado el delete') }
